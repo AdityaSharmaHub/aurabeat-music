f7aa514be21f3cce7cf35af6591aba20d225e766
Aditya Sharma <adisharma4857@gmail.com>
2024-02-15 19:53:26 +0530

    removed comments

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -11,10 +11,6 @@ const AroundYou = () => {
     const { activeSong, isPlaying } = useSelector((state) => state.player);
     const { data, isFetching, error } = useGetSongsByCountryQuery(country);
 
-    // useEffect(() => {
-    //     axios.get(`https://geo.ipify.org/api/v2/country?apiKey=at_5PDB1TVfaBUwyZYVQ4iQFBDTef1uS&ipAddress=8.8.8.8`).then((res) => setCountry(res?.data?.location?.country)).catch((err) => console.log(err)).finally(() => setLoading(false));
-    // }, [country]);
-
     if(isFetching && loading) return <Loader title="Loading songs around you" />;
 
     if(error & country) return <Error/>;
